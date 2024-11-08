796de87f84a7f0366c46e002f80f6e9fe234c07a
joaogcr <joao.nribeiro@sptech.school>
2024-11-07 22:21:37 -0300

    Começo de código para editar os dados

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -22,6 +22,15 @@ function cadastrar(nome, email, senha, telefone, data) {
     return database.executar(instrucaoSql);
 }
 
+function editar(novosDados, idAviso) {
+    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editar(): ", novosDados, id);
+    var instrucaoSql = `
+        UPDATE usuario SET email = '${email}' WHERE id = ${id};
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
     autenticar,
     cadastrar
